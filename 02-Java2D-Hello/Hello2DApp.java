df5fff5af912c1236d5a583f85960239029121dc
Th-brgs <98922950+Th-brgs@users.noreply.github.com>
2022-03-05 17:41:38 -0300

    Update Hello2DApp.java

--- 02-Java2D-Hello/Hello2DApp.java
+++ 02-Java2D-Hello/Hello2DApp.java
@@ -18,12 +18,12 @@ class Hello2DFrame extends JFrame {
             }
         );
         this.setTitle("Java2D - Hello World!");
-        this.setSize(350, 350);
+        this.setSize(450, 450);
+	this.getContentPane().setBackground(Color.red);
         this.setVisible(true);
     }
 
-    public void paint (Graphics g) {
-        super.paint(g);
+    void drawLine (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         g2d.setPaint(Color.blue);
         int w = getWidth();
@@ -31,4 +31,17 @@ class Hello2DFrame extends JFrame {
         g2d.drawLine(0,0, w,h);
         g2d.drawLine(0,h, w,0);
     }
+    void drawRectangles(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        // code to draw rectangles goes here...
+	g2d.setColor(Color.YELLOW);
+        g2d.drawRect(205,205,40,40);
+	g2d.fillRect(205,205,40,40);
+    }
+    public void paint (Graphics g) {
+        super.paint(g);
+	drawRectangles(g);
+	drawLine(g);
+    }
+
 }
